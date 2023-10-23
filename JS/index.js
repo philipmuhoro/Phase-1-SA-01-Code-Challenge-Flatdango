73085fb773cb886ae237cbeec178e338c2852879
philipwanjiku <muhorophilip7@gmail.com>
2023-10-23 11:16:46 +0300

    DOM elements

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+// DOM elements
+const movieDetails = document.getElementById("movie-details");
+const moviePoster = document.getElementById("movie-poster");
+const movieTitle = document.getElementById("movie-title");
+const movieRuntime = document.getElementById("movie-runtime");
+const movieShowtime = document.getElementById("movie-showtime");
+const availableTickets = document.getElementById("available-tickets");
+const buyTicketButton = document.getElementById("buy-ticket");
+const movieList = document.getElementById("films");
