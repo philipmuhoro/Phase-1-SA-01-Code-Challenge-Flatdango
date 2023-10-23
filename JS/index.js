213b2941f3a4a536f93c2a7b2f459c121890baec
philipwanjiku <muhorophilip7@gmail.com>
2023-10-23 11:20:10 +0300

    Function to select movie

--- JS/index.js
+++ JS/index.js
@@ -7,3 +7,16 @@ const movieShowtime = document.getElementById("movie-showtime");
 const availableTickets = document.getElementById("available-tickets");
 const buyTicketButton = document.getElementById("buy-ticket");
 const movieList = document.getElementById("films");
+
+let selectedMovie = null;
+
+// Function to update movie details when a movie is selected
+function updateMovieDetails(movie) {
+    movieDetails.style.display = "block";
+    moviePoster.src = movie.poster;
+    movieTitle.textContent = movie.title;
+    movieRuntime.textContent = `${movie.runtime} minutes`;
+    movieShowtime.textContent = movie.showtime;
+    availableTickets.textContent = movie.capacity - movie.tickets_sold;
+    selectedMovie = movie;
+}
