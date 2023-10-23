74db46478bb0314686a5998432347a337b3c7dbd
philipwanjiku <muhorophilip7@gmail.com>
2023-10-23 11:23:43 +0300

    Function to create movie list

--- JS/index.js
+++ JS/index.js
@@ -20,3 +20,27 @@ function updateMovieDetails(movie) {
     availableTickets.textContent = movie.capacity - movie.tickets_sold;
     selectedMovie = movie;
 }
+
+// Function to create a list item with movie details
+function createMovieListItem(movie) {
+    const li = document.createElement("li");
+    li.dataset.id = movie.id;
+    li.dataset.title = movie.title;
+    li.dataset.runtime = movie.runtime;
+    li.dataset.showtime = movie.showtime;
+    li.dataset.capacity = movie.capacity;
+    li.dataset.tickets_sold = movie.tickets_sold;
+
+    const posterImg = document.createElement("img");
+    posterImg.src = movie.poster;
+    posterImg.alt = movie.title;
+
+    const movieTitleLink = document.createElement("a");
+    movieTitleLink.href = "#";
+    movieTitleLink.textContent = movie.title;
+
+    li.appendChild(posterImg);
+    li.appendChild(movieTitleLink);
+
+    return li;
+}
