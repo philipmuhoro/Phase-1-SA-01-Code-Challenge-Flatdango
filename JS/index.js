cdc5ad1b904dc4fcf287f203e0ff487cce09bc18
philipwanjiku <muhorophilip7@gmail.com>
2023-10-25 15:46:37 +0300

    Completed Assignment

--- JS/index.js
+++ JS/index.js
@@ -1,91 +1,62 @@
-// DOM elements
-const movieDetails = document.getElementById("movie-details");
-const moviePoster = document.getElementById("movie-poster");
-const movieTitle = document.getElementById("movie-title");
-const movieRuntime = document.getElementById("movie-runtime");
-const movieShowtime = document.getElementById("movie-showtime");
-const availableTickets = document.getElementById("available-tickets");
-const buyTicketButton = document.getElementById("buy-ticket");
-const movieList = document.getElementById("films");
+// Define a function to fetch movie details and update the DOM
+function fetchMovieDetails(movieId) {
+    fetch(`https://my-json-server.typicode.com/philipmuhoro/Phase-1-SA-01-Code-Challenge-Flatdango/films/${movieId}`)
+        .then((response) => response.json())
+        .then((data) => {
+            // Update the DOM with movie details
+            const posterImg = document.getElementById('movie-poster');
+            const title = document.getElementById('movie-title');
+            const runtime = document.getElementById('movie-runtime');
+            const showtime = document.getElementById('movie-showtime');
+            const availableTickets = document.getElementById('movie-tickets');
+            const description = document.getElementById('movie-description');
+            const buyTicketButton = document.getElementById("buy-ticket");
 
-let selectedMovie = null;
+            posterImg.src = data.poster;
+            title.textContent = data.title;
+            runtime.textContent = `Runtime: ${data.runtime} minutes`;
+            showtime.textContent = `Showtime: ${data.showtime}`;
+            availableTickets.textContent = `Available Tickets: ${data.capacity - data.tickets_sold}`;
+            description.textContent = data.description;
+            
+            
+            
 
-// Function to update movie details when a movie is selected
-function updateMovieDetails(movie) {
-    movieDetails.style.display = "block";
-    moviePoster.src = movie.poster;
-    movieTitle.textContent = movie.title;
-    movieRuntime.textContent = `${movie.runtime} minutes`;
-    movieShowtime.textContent = movie.showtime;
-    availableTickets.textContent = movie.capacity - movie.tickets_sold;
-    selectedMovie = movie;
+            // Set the selectedMovie to the current movie data
+            selectedMovie = data;
+        });
 }
 
-// Function to create a list item with movie details
-function createMovieListItem(movie) {
-    const li = document.createElement("li");
-    li.dataset.id = movie.id;
-    li.dataset.title = movie.title;
-    li.dataset.runtime = movie.runtime;
-    li.dataset.showtime = movie.showtime;
-    li.dataset.capacity = movie.capacity;
-    li.dataset.tickets_sold = movie.tickets_sold;
+// Define a function to fetch the list of movies and populate the menu
+function fetchMovieList() {
+    fetch('https://my-json-server.typicode.com/philipmuhoro/Phase-1-SA-01-Code-Challenge-Flatdango/films')
+        .then((response) => response.json())
+        .then((data) => {
+            // Get the menu element
+            const menu = document.getElementById('films');
+            const movieListHTML = data.map(movie => {
+                return `<div class="movie-item" onclick="fetchMovieDetails(${movie.id})">${movie.title}</div>`;
+            }).join('');
 
-    const posterImg = document.createElement("img");
-    posterImg.src = movie.poster;
-    posterImg.alt = movie.title;
-
-    const movieTitleLink = document.createElement("a");
-    movieTitleLink.href = "#";
-    movieTitleLink.textContent = movie.title;
-
-    li.appendChild(posterImg);
-    li.appendChild(movieTitleLink);
-
-    return li;
+            // Populate the menu with movie titles
+            menu.innerHTML = movieListHTML;
+        });
 }
 
-// Function to handle ticket purchase
+// Function to handle the "Buy Ticket" button click
 function buyTicket() {
-    if (selectedMovie && parseInt(availableTickets.textContent) > 0) {
-        const newAvailableTickets = parseInt(availableTickets.textContent) - 1;
-        availableTickets.textContent = newAvailableTickets;
+    if (selectedMovie && parseInt(availableTickets.textContent.split(' ')[2]) > 0) {
+        const newAvailableTickets = parseInt(availableTickets.textContent.split(' ')[2]) - 1;
+        availableTickets.textContent = `Available Tickets: ${newAvailableTickets}`;
         selectedMovie.tickets_sold++;
-    }
-}
 
-// Function to fetch movie data from the JSON server
-function fetchMovieData() {
-    fetch('https://my-json-server.typicode.com/philipmuhoro/Phase-1-SA-01-Code-Challenge-Flatdango/films') // Change this URL when I start deploying.
-        .then(response => response.json())
-        .then(data => {
-            if (data && data.length > 0) {
-                const firstMovie = data[0]; // Get the first movie
-                updateMovieDetails(firstMovie);
-
-                data.forEach(movie => {
-                    const li = createMovieListItem(movie);
-                    movieList.appendChild(li);
-                });
-            }
-        })
-        .catch(error => console.error("Error fetching movie data: " + error));
-}
-
-// Event listener for selecting movie 
-movieList.addEventListener("click", (event) => {
-    if (event.target.tagName === "A") {
-        const movieId = event.target.parentElement.dataset.id;
-        const selectedMovie = data.find(movie => movie.id === movieId);
-        if (selectedMovie) {
-            updateMovieDetails(selectedMovie);
+        if (newAvailableTickets === 0) {
+            alert("Tickets are sold out!");
         }
     }
-});
+}
 
-// Event listener for purchasing tickets
-buyTicketButton.addEventListener("click", buyTicket);
 
-// Fetch movie data when the page loads
-fetchMovieData();
 
+// Fetch movie details and movie list when the page loads
+fetchMovieList();
